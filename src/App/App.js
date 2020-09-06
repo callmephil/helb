430ab18edc6f91eda8d205dfca3a60f023927a97
Callmephil <phil19-94@hotmail.fr>
2020-09-06 14:15:26 +0300

    Added form sidebar, non working example

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import ResponsiveContainer from "./Components/Navbar";
 import Footer from "./Components/Footer";
@@ -16,25 +16,29 @@ import "./App.css";
 import HealthCarePage from "../Pages/VolunteersPage/HealthCarePage";
 import RenovationsPage from "../Pages/VolunteersPage/RenovationsPage";
 import HousingPage from "../Pages/VolunteersPage/HousingPage";
+import FormContainer from "./Components/FormContainer";
 
 function App() {
-
+  const [visible, setVisible] = useState(false);
+  const [formComponent, setFormComponent] = useState();
   return (
     <Fragment>
       <ResponsiveContainer>
-        <Container className="main-container">
-          <Switch>
-            <Route path="/" exact render={() => <HomePage />} />
-            <Route path="/organizations" exact render={() => <OrganizationsPage />} />
-            <Route path="/campaigns" exact render={() => <CampaignsPage />} />
-            <Route path="/events" exact render={() => <EventsPage />} />
-            <Route path="/memorial" exact render={() => <MemorialPage />} />
-            <Route path="/volunteers/health" exact render={() => <HealthCarePage />} />
-            <Route path="/volunteers/renovation" exact render={() => <RenovationsPage />} />
-            <Route path="/volunteers/housing" exact render={() => <HousingPage />} />
-            <Route path="/profile" exact render={() => <ProfilePage />} />
-          </Switch>
-        </Container>
+        <FormContainer visible={visible} setVisible={setVisible} content={formComponent}>
+          <Container className="main-container">
+            <Switch>
+              <Route path="/" exact render={() => <HomePage />} />
+              <Route path="/organizations" exact render={() => <OrganizationsPage />} />
+              <Route path="/campaigns" exact render={() => <CampaignsPage />} />
+              <Route path="/events" exact render={() => <EventsPage />} />
+              <Route path="/memorial" exact render={() => <MemorialPage />} />
+              <Route path="/volunteers/health" exact render={() => <HealthCarePage />} />
+              <Route path="/volunteers/renovation" exact render={() => <RenovationsPage />} />
+              <Route path="/volunteers/housing" exact render={() => <HousingPage />} />
+              <Route path="/profile" exact render={() => <ProfilePage setFormComponent={setFormComponent} setVisible={setVisible} />} />
+            </Switch>
+          </Container>
+        </FormContainer>
       </ResponsiveContainer>
       <Footer />
     </Fragment>
--- src/Pages/Profile/ProfilePage.js
+++ src/Pages/Profile/ProfilePage.js
@@ -1,10 +1,79 @@
-import React from "react";
-import { Grid, Header, Button, Container } from "semantic-ui-react";
+import React , { useState } from "react";
+import {
+  Grid,
+  Header,
+  Button,
+  Container,
+  Image,
+  Segment,
+  Form,
+  Select,
+  Input,
+} from "semantic-ui-react";
 import ProfileCard from "./Components/ProfileCard";
 import MyContributionTable from "./Components/MyContributionTable";
 import PageHeadings from "../Components/PageHeadings";
 
-function ProfileContributionAction() {
+const options = [
+  { key: "c", text: "Campaign", value: "campaign" },
+  { key: "e", text: "Event", value: "event" },
+  { key: "o", text: "Other", value: "other" },
+];
+
+
+// detect type
+// wait for url to trigger second column
+function Example() {
+  const [contributionType, setContributionType] = useState('');
+  const [websiteUrl, setWebsiteUrl] = useState('');
+  const [validation, setValidation] = useState(false);
+  const [type, ] = useState('')
+
+  const handleSubmit = () => {
+
+    // const type = getTypeFromUrl(link);
+    setValidation(true);
+  }
+
+
+  return (
+    <PageHeadings title="Simple Contribution" description={`Campaigns, Events`}>
+      <Container style={{ padding: "1rem", textAlign: "left" }}>
+        <Grid stackable columns={2}>
+          <Grid.Column>
+            <Form onSubmit={() => handleSubmit()}>
+                <Form.Select
+                  required
+                  value={contributionType}
+                  options={options}
+                  label="Contribution Type"
+                  placeholder="Contribution Type"
+                  onChange={(e) => setContributionType(e.target.value)}
+                />
+                <Form.Input
+                  required
+                  label="Website URL"
+                  value={websiteUrl}
+                  placeholder="Website URL"
+                  onChange={(e) => setWebsiteUrl(e.target.value)}
+                  id="form-subcomponent-shorthand-input-last-name"
+                />
+                <Button type='submit'>Submit</Button>
+            </Form>
+          </Grid.Column>
+          <Grid.Column>
+            {validation &&
+              <CardWrapper key={link} type={type} url={link} labels={labels} />
+          }
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </PageHeadings>
+  );
+};
+
+function ProfileContributionAction({ setFormComponent, setVisible }) {
+  const example = <Example />
   return (
     <Grid celled="internally" columns="equal" stackable>
       <Grid.Row textAlign="center">
@@ -13,9 +82,16 @@ function ProfileContributionAction() {
             Simple Contribution
           </Header>
           <p style={{ fontSize: "1em" }}>
-            You found something interesting you would like to share? Click the button below. best fit for events and memorials.
+            You found something interesting you would like to share? Click the button below. best
+            fit for events and memorials.
           </p>
-          <Button size="huge" color="blue">
+          <Button
+            size="huge"
+            color="blue"
+            onClick={() => {
+              setVisible(true);
+              setFormComponent(example);
+            }}>
             Contribute
           </Button>
         </Grid.Column>
@@ -24,8 +100,8 @@ function ProfileContributionAction() {
             Advanced Contribution
           </Header>
           <p style={{ fontSize: "1em" }}>
-            If you're an activist or an advanced user, you can contribute by adding Organizations and
-            much more...
+            If you're an activist or an advanced user, you can contribute by adding Organizations
+            and much more...
           </p>
           <Button size="huge" color="black">
             Contribute
@@ -36,7 +112,7 @@ function ProfileContributionAction() {
   );
 }
 
-export default function ProfilePage() {
+export default function ProfilePage({ setFormComponent, setVisible }) {
   return (
     <Container style={{ padding: "4em 0em" }}>
       <PageHeadings title="Profile Page" description={``}>
@@ -55,7 +131,10 @@ export default function ProfilePage() {
               <PageHeadings
                 title="Contribute"
                 description={`Your Contributions Helps Helb helping others by providing them rapid and reliable informations. Choose below how you would like to help helb improve its content.`}>
-                <ProfileContributionAction />
+                <ProfileContributionAction
+                  setFormComponent={setFormComponent}
+                  setVisible={setVisible}
+                />
               </PageHeadings>
             </Grid.Column>
           </Grid.Row>
