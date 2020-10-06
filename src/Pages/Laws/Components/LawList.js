fbe8a4942490608121581c648455ffd052cc76ab
PhilMakzoume <phil@polarstork.com>
2020-10-06 23:01:41 +0300

    Added Lawlist Naive Filter (Todo some refactoring) related #27

--- src/Pages/Laws/Components/LawList.js
+++ src/Pages/Laws/Components/LawList.js
@@ -2,30 +2,6 @@ import React from "react";
 import _ from "lodash";
 import { Accordion, Label } from "semantic-ui-react";
 
-const laws = [
-  {
-    title: "First Law",
-    content: "First Content Law",
-    definition: "This is the first law",
-    usages: "this is applied all the time",
-    tags: ["health", "energy"],
-  },
-  {
-    title: "Second Law",
-    content: "Second Content Law",
-    definition: "This is the Second law",
-    usages: "this is applied all the time",
-    tags: ["health", "energy"],
-  },
-  {
-    title: "Third Law",
-    content: "Third Content Law",
-    definition: "This is the Third law",
-    usages: "this is applied all the time",
-    tags: ["health", "energy"],
-  },
-];
-
 const lawAccordionTab = ({ key, title, content, definition, usages, tags }) => {
   const sub_sections = [
     { key: `definition-${key}`, title: "Definition", content: definition },
@@ -51,10 +27,14 @@ const lawAccordionTab = ({ key, title, content, definition, usages, tags }) => {
   return { key: `panel-${key}`, title: title, content: { content: SubPanel } };
 };
 
-const rootPanels = _.map(laws, (lawContent, key) => {
-  return lawAccordionTab({ key, ...lawContent });
-});
+function LawList({laws}) {
+  const rootPanels = _.map(laws, (lawContent, key) => {
+    return lawAccordionTab({ key, ...lawContent });
+  });
 
-const LawList = () => <Accordion fluid defaultActiveIndex={0} panels={rootPanels} styled />;
+  return (
+    <Accordion fluid defaultActiveIndex={0} panels={rootPanels} styled />
+  )
+}
 
 export default LawList;
--- src/Pages/Laws/LawPage.js
+++ src/Pages/Laws/LawPage.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import { Container } from 'semantic-ui-react'
-import PageHeadings from '../Components/PageHeadings'
-import LawList from './Components/LawList'
+import React, { useState } from "react";
+import { Container } from "semantic-ui-react";
+import PageHeadings from "../Components/PageHeadings";
+import SearchExampleCategory from "./Components/Filter";
+import LawList from "./Components/LawList";
+
+const initialState = [
+  {
+    title: "First Law",
+    content: "First Content Law",
+    definition: "This is the first law",
+    usages: "this is applied all the time",
+    tags: ["health", "energy"],
+  },
+  {
+    title: "Second Law",
+    content: "Second Content Law",
+    definition: "This is the Second law",
+    usages: "this is applied all the time",
+    tags: ["health", "energy"],
+  },
+  {
+    title: "Third Law",
+    content: "Third Content Law",
+    definition: "This is the Third law",
+    usages: "this is applied all the time",
+    tags: ["health", "energy"],
+  },
+];
 
 export default function LawPage() {
-    return (
+  const [state, setstate] = useState(initialState)
+
+  return (
     <Container style={{ padding: "4em 0em" }}>
       <PageHeadings
         title="Laws"
@@ -13,10 +40,12 @@ export default function LawPage() {
           ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
           sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.`}>
-      </PageHeadings>
+          est laborum.`}
+      />
 
-      <LawList />
+      <SearchExampleCategory source={initialState} _setResults={setstate} />
+
+      <LawList laws={state} />
     </Container>
-    )
+  );
 }
